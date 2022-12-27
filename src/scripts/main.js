e360f7ba193fecaeb1026a98c8b7ad235f6391d5
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2022-12-27 20:02:20 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded' , function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(e) {
+        e.preventDefault()
+        let numeroMaximo = document.getElementById('numero-maximo').value
+        numeroMaximo = parseInt(numeroMaximo)
+
+        let numeroAleatorio = Math.random() * numeroMaximo
+        document.getElementById('resultado').innerText = Math.floor(numeroAleatorio + 1)
+        document.querySelector('.resultado').style.display = 'block'
+    })
+})
